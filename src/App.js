20aed383d28303ab34a4803409e68f9acd52a1d2
suguloga <lcsugit@gmail.com>
2018-10-18 22:58:13 -0700

    Create initial line plot

--- src/App.js
+++ src/App.js
@@ -1,9 +1,100 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { ResponsiveLine } from "@nivo/line";
 
 class App extends Component {
+  state = {
+    data: [
+      {
+        id: "School",
+        data: [{ x: 1, y: 2 }, { x: 2, y: 4 }, { x: 3, y: 9 }, { x: 4, y: 16 }]
+      },
+      {
+        id: "Work",
+        data: [{ x: 1, y: 2 }, { x: 2, y: 3 }, { x: 3, y: 4 }, { x: 4, y: 5 }]
+      }
+    ]
+  };
   render() {
+    console.log(this.state.data);
     return (
-        <div>Hello world!</div>
+      <ResponsiveLine
+        data={this.state.data}
+        margin={{
+          top: 50,
+          right: 110,
+          bottom: 50,
+          left: 60
+        }}
+        xScale={{
+          type: "linear",
+          stacked: true,
+          min: "auto",
+          max: "auto"
+        }}
+        yScale={{
+          type: "linear",
+          stacked: true,
+          min: "auto",
+          max: "auto"
+        }}
+        minY="auto"
+        maxY="auto"
+        stacked={true}
+        axisBottom={{
+          orient: "bottom",
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "Time",
+          legendOffset: 36,
+          legendPosition: "center"
+        }}
+        axisLeft={{
+          orient: "left",
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "count",
+          legendOffset: -40,
+          legendPosition: "center"
+        }}
+        dotSize={10}
+        dotColor="inherit:darker(0.3)"
+        dotBorderWidth={2}
+        dotBorderColor="#ffffff"
+        enableDotLabel={true}
+        dotLabel="y"
+        dotLabelYOffset={-12}
+        animate={true}
+        motionStiffness={90}
+        motionDamping={15}
+        legends={[
+          {
+            anchor: "bottom-right",
+            direction: "column",
+            justify: false,
+            translateX: 100,
+            translateY: 0,
+            itemsSpacing: 0,
+            itemDirection: "left-to-right",
+            itemWidth: 80,
+            itemHeight: 20,
+            itemOpacity: 0.75,
+            symbolSize: 12,
+            symbolShape: "circle",
+            symbolBorderColor: "rgba(0, 0, 0, .5)",
+            effects: [
+              {
+                on: "hover",
+                style: {
+                  itemBackground: "rgba(0, 0, 0, .03)",
+                  itemOpacity: 1
+                }
+              }
+            ]
+          }
+        ]}
+      />
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+  <div style={{ height: "600px", width: "800px" }}>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
